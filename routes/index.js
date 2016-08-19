aa3bc0b43ab0c1ac4958ae90fb0dd0157349adf9
Jacob Rafati-Heravi <jacobrafati@Jacobs-MacBook-Pro.local>
2016-08-18 17:17:01 -0700

    minor bug about varibale defenition

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var Flowers = require('../models/flower');
+//var Flowers = require('../models/flower');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //Flowers.find(function(err,docs){
-    res.render('menu/index', { title: 'Jahnetics', flowers:docs});
+    res.render('menu/index', { title: 'Jahnetics'});
   //});
 });
 
